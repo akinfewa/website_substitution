2815ad688f6d0d8b92b7aaa99f0f261e9c36fc40
amine <amine.ghoumid@viacesi.fr>
2020-07-02 15:00:32 +0200

    notifications view

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -19,6 +19,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
@@ -42,7 +43,8 @@
 
     <div class="collapse navbar-collapse text-center" id="navbarSupportedContent">
         <div class="col-sm">
-            <ul class="navbar-nav mr-auto">
+
+            <ul class="navbar-nav">
 
                 <!-- Compare NavBar connect or no -->
 
@@ -95,19 +97,31 @@
                     <a class="nav-link" href="/contact">Contact</a>
                 </li>
             @endif
+        <!-- Notification -->
             </ul>
-{{--        </div>--}}
-{{--        <!-- Recherche -->--}}
-{{--        <form class="form-inline my-2 my-lg-0">--}}
-
-{{--            <input class="form-control mr-sm-2" type="search" placeholder="" aria-label="">--}}
-
-{{--            <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">Chercher</button>--}}
-
-{{--        </form>--}}
-{{--    </div>--}}
-        </nav>
-        </header>
+        </div>
+    </div>
+                    <form class="form-inline" >
+                        <button type="button" style="margin-right: 25px" class="btn btn-outline-success my-2 my-sm-0" data-toggle="modal" data-target="#modal"><i class="far fa-bell" ></i></button>
+                        <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title">Centre de notification</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">x</span></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p>ICI THOMAS</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+    </nav>
+</header>
 
     <!-- Footer -->
 
