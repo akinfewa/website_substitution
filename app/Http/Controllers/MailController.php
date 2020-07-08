17c86dc57cf253a6547568cf5660c02490ebc224
laridsen <damien@zaroax.eu>
2020-07-08 15:15:27 +0200

    Mail (fonctionnel)

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -10,12 +10,12 @@ use App\Http\Requests;
 class MailController extends Controller{
 
   private $tbCTX = [
-          "validation"=>["Objet"=>"Confirmation de commande", "Contenu"=>"<h1>Bonjour</h1><br>Vous avez une visière"],
+          "validation"=>["Objet"=>"Confirmation de commande", "Contenu"=>"Bonjour, vous venez de commander une visière"],
           "annulation"=>["Objet"=>"Annulation de commande", "Contenu"=>"<h1>Bonjour</h1><br>Vous avez plus une visière"],
   ];
 
 public function basic_email($mailDestinataire, $userName, $contexte) {
-    $data = array('name'=>"Zaroax", "body" => $this->tbCTX[$contexte]['Contenu']);
+    $data = array("body" => $this->tbCTX[$contexte]['Contenu']);
 
     try{
         Mail::send('mail', $data, function($message) use ($mailDestinataire, $userName, $contexte) {
